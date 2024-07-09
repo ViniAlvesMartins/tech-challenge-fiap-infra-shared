dc4bd3221eaaf10c2bddb9e4fb1daa6078dc34d9
MarcosBan <marcos.mps10@gmail.com>
2024-07-09 13:20:16 -0300

    Add output data

--- output.tf
+++ output.tf
@@ -2,8 +2,16 @@ output "sqs_payment_production_order_queue_arn" {
     value = aws_sqs_queue.from_payment_production_order_queue.arn
 }
 
-output "sqs_to_production_order_queue" {
+output "sqs_payment_production_order_queue_url" {
+    value = aws_sqs_queue.from_payment_production_order_queue.url
+}
+
+output "sqs_to_production_order_queue_arn" {
     value = aws_sqs_queue.to_production_order_queue.arn
+}
+
+output "sqs_to_production_order_queue_url" {
+    value = aws_sqs_queue.to_production_order_queue.url
 }
 
 output "sns_update_order_status_arn" {
